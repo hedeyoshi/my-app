93dc7c55fa56ee2dc0ddaa449ae3b9a04b4c11af
hedeyoshi <h158cm@gmail.com>
2019-06-05 11:24:31 +0900

    scss使用可

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,17 +95,19 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h1, h2 {
   font-weight: normal;
 }
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    background-color: #dffffd;
+    padding: 10px;
+  }
 }
 a {
   color: #42b983;
