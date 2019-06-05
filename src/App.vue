8cf016a8cbdcc924b74b591c3aade3845a05f1aa
hedeyoshi <h158cm@gmail.com>
2019-06-05 12:54:21 +0900

    store追記中

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,50 @@
 <template>
   <div id="app">
+    <h2>引数なし</h2>
+    <ol>
+      <li>{{ count }}</li>
+      <li>{{ max }}</li>
+    </ol>
+    <h2>引数付き</h2>
+    <ol>
+      <li>{{ itemA }}</li>
+      <li>{{ itemB(1) }}</li>
+      <li>{{ nameA }}</li>
+      <li>{{ nameB(1) }}</li>
+    </ol>
+    <!-- <p>
+      引数付きゲッターの itemB / nameB の書き方は便利ですが、結果はキャッシュされません。 算出プロパティを通さない場合も同じです。 何度も使用していたり、コンポーネントの仮想 DOM に変化があるたびに呼び出されてしまうため、コストの高い算出処理をしている場合には注意しましょう！
+    </p> -->
     <img src="./assets/logo.png">
     <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  computed: {
+    // 引数なしゲッター
+    count() { return this.$store.getters.count },   // 1
+    max()   { return this.$store.getters.max },     // 2
+    // 引数付きゲッター
+    itemA() { return this.$store.getters.item(1) }, // 1 👍 いいね
+    itemB() { return this.$store.getters.item },    // 2 👎 よくないね
+    nameA() { return this.$store.getters.name(1) }, // 3 👍 いいね
+    nameB() { return this.$store.getters.name },    // 4 👎 よくないね
   }
-}
+
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -2,12 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from '@/store.js'
 
 Vue.config.productionTip = false
+
+console.log(store.state.count) // -> 0
+// incrementをコミットする
+store.commit('increment')
+// もう一度アクセスしてみるとカウントが増えている
+console.log(store.state.count) // -> 1
+
+console.log(store.getters.count)
+console.log(store.getters.max)
+console.log(store.getters.item(1))
+console.log(store.getters.name(1))
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store, // storeを登録
+  render: h => h(App),
   components: { App },
   template: '<App/>'
 })
